51d62edab9fe2f0af0514440ee2d433b2be8417c
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-06-01 11:22:18 +0530

    user database task

---
+++ remix-solidity/UserDatabase.sol
@@ -0,0 +1,12 @@
+// Main Contract - Controller
+// Storage Contract - Storage
+
+// User Database - Name, Age 
+// Events New user and New Admin
+
+// At Storage - Only State Variables and Getter and Setters
+            // Only accessed by controller 
+
+// At Controller - Anyone can register
+                    // Only admin can deploy new storage contract 
+                    // and update storage conract
